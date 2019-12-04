3e7de0c8f787b008216882a9623787c8c5a886f2
nano <15460626@itcolima.edu.mx>
2019-12-04 00:45:59 -0600

    ya quedo

---
+++ proyecto.py
@@ -0,0 +1,38 @@
+from flask import Flask,render_template
+import pygal
+from pygal.style import Style
+import json
+import time
+import pandas as pd
+app = Flask(__name__)
+
+custom_style = Style(
+        colors=('#991515','#1cbc7c'),
+        background='#d2ddd9'
+        )
+
+
+@app.route("/")
+def hello():
+    df = pd.read_csv("datos.csv")
+    return df.to_html()
+# -------------------------------------------
+# Charting route which displays the bar chart
+# -------------------------------------------
+
+@app.route("/graficabar")
+def bar():
+    with open('bar.json','r') as bar_file:
+        data = json.load(bar_file)
+    chart = pygal.Bar(style = custom_style)
+    intentos_list = [x['intentos'] for x in data]
+    puntaje_list = [x['puntaje'] for x in data]
+    chart.add('Puntaje',puntaje_list)
+    chart.add('intentos',intentos_list)
+    chart.x_labels = [x['name'] for x in data]
+    chart.render_to_file('static/images/Grafica.svg')
+    img_url = 'static/images/grafica.svg?cache=' + str(time.time())
+    return render_template('nano.html',image_url = img_url)
+
+if __name__=="__main__":
+    app.run()
